98526b933d0d4aab5c1393484c86d4da53915ea6
Jaeyeon Kim <jaeyeonling@gmail.com>
2019-01-14 00:22:16 +0900

    initialize commit

---
+++ precache-manifest.bf42b34184ef535dc5ffd7e98ee15437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d8f1a8615e961c4dcc9",
+    "url": "/css/app.02797e56.css"
+  },
+  {
+    "revision": "7d8f1a8615e961c4dcc9",
+    "url": "/js/app.d954e4d1.js"
+  },
+  {
+    "revision": "8be41589914797d97f79",
+    "url": "/js/chunk-vendors.48bbcde3.js"
+  },
+  {
+    "revision": "d2cd5930672412d22ad79f13758f9ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
